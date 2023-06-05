ed46ae4625adf8e8cf03f9c7efab8d2d02087a80
ThanhDoTuan <thanhdttb2001@gmail.com>
2023-06-05 13:55:04 +0700

    add data from web

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,6 @@
 import { json } from 'body-parser';
 import db from '../models/index';
+import CRUDService from '../services/CRUDService';
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll();
@@ -21,7 +22,17 @@ let getAboutPage = (req, res) => {
 //     key: '',
 //     value: ''
 // }
+let getCRUD = (req,res) =>{
+    return res.render('crud.ejs');
+}
+let postCRUD = async (req,res) =>{
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message);
+    return res.send('post crud from server');
+}
 module.exports = {
     getHomePage: getHomePage,
-    getAboutPage: getAboutPage
+    getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD
 }
---
+++ src/services/CRUDService.js
@@ -0,0 +1,37 @@
+import bcrypt from 'bcryptjs';
+import db from '../models/index';
+const salt = bcrypt.genSaltSync(10);
+
+let createNewUser = async (data) =>{
+    return new Promise(async(resolve,reject) =>{
+            try {
+        let hashPassWordFromBcrypt = await hashUserPassword(data.password);
+        await db.User.create({
+            email: data.email,
+    password: hashPassWordFromBcrypt,
+    firstName: data.firstName,
+    lastName: data.lastName,
+    address: data.address,
+    phoneNumber: data.phoneNumber,
+    gender: data.gender === '1' ? true : false,
+    roleId: data.roleId,
+        })
+        resolve('Ok create successfully!')        
+            } catch (e) {
+                    reject(e);
+            }
+    })
+}
+let hashUserPassword = (password) =>{
+    return new Promise(async(resolve, reject) => {
+       try {
+let hashPassWord = await bcrypt.hashSync(password, salt);
+        resolve(hashPassWord);
+       } catch (error) {
+            reject(error);
+       }
+    })
+}
+module.exports ={
+    createNewUser: createNewUser
+}
